2289547beb902d7fed475f4d611bb95378447cab
arplx <93895162+arplx@users.noreply.github.com>
2024-01-05 19:34:13 +1200

    Create data.js

---
+++ data.js
@@ -0,0 +1,13 @@
+var data = [
+    {
+        image: 'https://link-to-your-image-1.jpg',
+        caption: 'Caption for post 1',
+        date: 'Date of post 1'
+    },
+    {
+        image: 'https://link-to-your-image-2.jpg',
+        caption: 'Caption for post 2',
+        date: 'Date of post 2'
+    },
+    // Add more posts as needed
+];
